1c4dc4c8469254f0871305cd41d03f33d4a718d6
Evelyne <namwoyoeve@gmail.com>
2017-04-25 18:36:50 +0300

    wrote a test to check whether the create_room function creates a room name successfully.

---
+++ dojo_rooms_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from allocate_rooms import Dojo
+
+class TestCreateRoom(unittest.TestCase):
+    def test_create_room_successfully(self):
+        my_class_instance = Dojo()
+        initial_room_count = len(my_class_instance.room_list)
+        blue_office = my_class_instance.create_room('Blue', 'office')
+        self.assertTrue(blue_office)
+        new_room_count = len(my_class_instance.room_list)
+        self.assertEqual(new_room_count - initial_room_count, 1)
+if __name__ == "__main__":
+    unittest.main()
