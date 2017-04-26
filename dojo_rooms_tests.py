02ded7f8c2e8f32319953298da40b27fce40a40f
Evelyne <namwoyoeve@gmail.com>
2017-04-26 12:02:57 +0300

    add the test case to create an office with multiple rooms

--- dojo_rooms_tests.py
+++ dojo_rooms_tests.py
@@ -1,14 +1,27 @@
 import unittest
 from allocate_rooms import Dojo
 
-class TestCreateRoomAddRoom(unittest.TestCase):
+class TestDojo(unittest.TestCase):
+    def setUp(self):
+        self.room = Dojo()
+
+    def test_dojo_instance(self):
+        self.room = Dojo()
+        self.assertIsInstance(self.room, Dojo, msg='The object should be an instance of the `Dojo` class')
+
     def test_create_room_successfully(self):
-        my_class_instance = Dojo()
-        initial_room_count = len(my_class_instance.room_list)
-        blue_office = my_class_instance.create_room('Blue', 'office')
+        initial_room_count = len(self.room.all_rooms)
+        blue_office = self.room.create_room('office', 'blue')
         self.assertTrue(blue_office)
-        new_room_count = len(my_class_instance.room_list)
+        new_room_count = len(self.room.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
+
+    def test_create_office_with_multiple_rooms_(self):
+        initial_room_count = len(self.room.all_rooms)
+        office_rooms = self.room.create_room('office','heep','purple','green' )
+        self.assertTrue(office_rooms)
+        rooms_added = len(self.room.all_rooms)
+        self.assertEqual(rooms_added - initial_room_count, 3)
+
 if __name__ == "__main__":
-    unittest.main()  #run main
-    print ('Program running')
+    unittest.main()
