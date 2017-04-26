7ea0c57e26e42a90e3fcd1335f7e5586bb925f93
Evelyne <namwoyoeve@gmail.com>
2017-04-26 22:40:44 +0300

    add a function to check length of list in a room and check for more space in a room

--- room.py
+++ room.py
@@ -1,15 +1,33 @@
 class Room(object):
-    def __init__(self, rname, rtype, max_no):
-        self.max_no = max_no
-        self.rtype =rtype
+    def __init__(self, rname):
         self.rname=rname
+        self.rtype = __class__.__name__
+        self.people_list = []
+
+    def add_person_list(self,pason):
+        self.people_list.append(pason)
+
+    def no_people(self):
+        return len(self.people_list)
+        
 class OfficeSpace(Room):
     def __init__(self, rname):
-        max_no=6
-        rtype='Office'
-        Room.__init__(self,rname,rtype,max_no)
+        super(OfficeSpace,self).__init__(rname)
+        self.max_no=6
+        self.rtype = __class__.__name__
+    
+    def more_space(self):
+        if len(self.people_list) < self.max_no:
+            return True
+
+        # Room.__init__(self,rname,rtype,max_no)
 class LivingSpace(Room):
     def __init__(self, rname):
-        max_no=4
-        rtype='Living Space'
-        Room.__init__(self,rname,rtype,max_no)
+        super(LivingSpace,self).__init__(rname)
+        self.max_no=4
+        self.rtype = __class__.__name__
+    def more_space(self):
+        if len(self.people_list) < self.max_no:
+            return True
+
+        # Room.__init__(self,rname,rtype,max_no)
