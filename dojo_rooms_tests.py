350d66a2b0038d13285bf484dcc51c9d03aa5273
Evelyne <namwoyoeve@gmail.com>
2017-04-26 12:34:17 +0300

    add a test to check whether a person has been added successfully

--- dojo_rooms_tests.py
+++ dojo_rooms_tests.py
@@ -16,12 +16,20 @@ class TestDojo(unittest.TestCase):
         new_room_count = len(self.room.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
 
-    def test_create_office_with_multiple_rooms_(self):
+    def test_create_room_type_with_multiple_rooms_(self):
         initial_room_count = len(self.room.all_rooms)
         office_rooms = self.room.create_room('office','heep','purple','green' )
         self.assertTrue(office_rooms)
         rooms_added = len(self.room.all_rooms)
         self.assertEqual(rooms_added - initial_room_count, 3)
+
+    def test_add_person_successfully(self):
+        initial_no_of_people = len(self.room.people)
+        people_add = self.room.add_person('eva','staff')
+        self.assertTrue(people_add)
+        no_of_people_added = len(self.room.people)
+        self.assertEqual(no_of_people_added - initial_no_of_people,1) 
+
 
 if __name__ == "__main__":
     unittest.main()
