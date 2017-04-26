ae6db97c835f60ef533fdb776074f7019b9e99d3
Evelyne <namwoyoeve@gmail.com>
2017-04-26 23:50:40 +0300

    debug the add person function

--- allocate_rooms.py
+++ allocate_rooms.py
@@ -1,3 +1,4 @@
+import random
 from room import OfficeSpace
 from room import LivingSpace
 from person import Staff
@@ -5,17 +6,17 @@ from person import Fellow
 class Dojo(object):
 
     def __init__(self):
-        self.people=[]
-        self.persons=[]
+        self.people = []
+        self.persons = []
         self.all_rooms = []
         self.rooms = []
-
+    """ this function creates a room of either office or living space, and appends the created room to a list rooms"""
     def create_room(self, rtype, *rname):
         if rtype == 'Office':
             for oname in rname:
                 office = OfficeSpace(rname)
                 self.rooms.append(office)
-                print(office.rname[0])
+                print(office.rname)
             #tuple_rooms = office.rname
             #for room in tuple_rooms:
                 self.all_rooms.append(office)
@@ -28,7 +29,7 @@ class Dojo(object):
                 self.all_rooms.append(room)
 
         return self.all_rooms
-
+    """This function adds a person to a room, an drandomly allocates the to availble office or space"""
 
     def add_person(self,position,gender,pname):
         if position == 'Fellow' and self.accommodation == True:
@@ -36,11 +37,17 @@ class Dojo(object):
         else:
             pa = Staff(pname,gender)
 
-        # free_rooms = []
+        free_rooms = []
         for room in self.rooms:
             if room.more_space():
                 room.add_person_list(pa)
-        print(room.people_list[0].pname)
+                for pips in room.people_list:
+                    return pips.pname
+                for i in room.rname:
+                    free_rooms.append(i)
+            a=random.choice(free_rooms)
+            print (a)
+        # print(room.people_list[0].pname)
                 # for frooms in room.rname:
                     # free_rooms.append(frooms)
 
@@ -77,6 +84,6 @@ class Dojo(object):
 
 
 a=Dojo()
-print (a.create_room('Office','gordy'))
-print(a.add_person('Staff','female','eva'))
+print (a.create_room('Office','gordy','hh','uyt'))
+print(a.add_person('Office','female','eva'))
 
