a8f5c070b4322103069526454dca3667db345f2a
Evelyne <namwoyoeve@gmail.com>
2017-04-27 14:25:24 +0300

    modify tests to create room type of multiple living rooms

--- dojo_rooms_tests.py
+++ dojo_rooms_tests.py
@@ -10,37 +10,44 @@ class TestDojo(unittest.TestCase):
         self.assertIsInstance(self.room, Dojo, msg='The object should be an instance of the `Dojo` class')
 
     def test_create_room_office(self):
-        initial_room_count = len(self.room.all_rooms)
+        initial_room_count = len(self.room.rooms)
         blue_office = self.room.create_room('office', 'blue')
         self.assertTrue(blue_office)
-        new_room_count = len(self.room.all_rooms)
+        new_room_count = len(self.room.rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
     def test_create_room_living(self):
-        initial_room_count = len(self.room.all_rooms)
+        initial_room_count = len(self.room.rooms)
         blue_living = self.room.create_room('livingspace', 'blue')
         self.assertTrue(blue_living)
-        new_room_count = len(self.room.all_rooms)
+        new_room_count = len(self.room.rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
 
     def test_create_room_type_with_multiple_office_rooms_(self):
-        initial_room_count = len(self.room.all_rooms)
+        initial_room_count = len(self.room.rooms)
         office_rooms = self.room.create_room('office','heep','purple','green' )
         self.assertTrue(office_rooms)
-        rooms_added = len(self.room.all_rooms)
+        rooms_added = len(self.room.rooms)
         self.assertEqual(rooms_added - initial_room_count, 3)
     def test_create_room_type_with_multiple_living_rooms_(self):
-        initial_room_count = len(self.room.all_rooms)
+        initial_room_count = len(self.room.rooms)
         living_rooms = self.room.create_room('livingspace','heep','purple','green' )
         self.assertTrue(living_rooms)
-        rooms_added = len(self.room.all_rooms)
+        rooms_added = len(self.room.rooms)
         self.assertEqual(rooms_added - initial_room_count, 3)
+
+    def test_room_type(self):
+        self.room.create_room('yelow', 'purple')
+        self.assertEqual(
+            'yellow', "Invalid Room Type. Room type can either be \'office\' or \'livingspace\'")
 
     def test_add_person_successfully(self):
         initial_no_of_people = len(self.room.people)
         people_add = self.room.add_person('Staff','Staff','female')
         self.assertTrue(people_add)
         no_of_people_added = len(self.room.people)
-        self.assertEqual(no_of_people_added - initial_no_of_people, 1) 
+        self.assertEqual(no_of_people_added - initial_no_of_people, 1)
+
+
 
 
 if __name__ == "__main__":
